68c7b8580d072be619e8ed8bb2b4221b89ff3168
Ang619 <angelos619@outlook.de>
2022-10-21 10:19:58 +0000

    basics created

---
+++ index.js
@@ -0,0 +1,13 @@
+// import
+
+constexpress =require('express');
+constapp = express();
+constPORT = 8080;
+
+// Listener
+app.listen(
+    PORT,
+    ()=> console.log(`Server ready at http://localhost:${PORT}`)
+);
+
+
